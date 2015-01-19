ab2a16a8fb50beb28df151cc20d018e76a8be642
WindowsGit <jamescheng16@gmail.com>
2015-01-19 09:08:00 +0100

    Add Category and User model

---
+++ iBuy/iBuy/iBuy/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace iBuy.Models
+{
+    public class Category
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+
+    }
+    public class CategoryDBContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ iBuy/iBuy/iBuy/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace iBuy.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public string Mail { get; set; }
+
+        public decimal PhoneNumber { get; set; }
+    }
+
+    public class UserDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
