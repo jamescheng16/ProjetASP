590c28fdaabce413f8f1aafc0f4f48df2f2c57a6
WindowsGit <jamescheng16@gmail.com>
2015-01-19 08:39:38 +0100

    create projet ibuy

--- WebApplication1/WebApplication1/Controllers/MoviesController.cs
+++ WebApplication1/WebApplication1/Controllers/MoviesController.cs
@@ -15,8 +15,17 @@ namespace WebApplication1.Controllers
         private MovieDBContext db = new MovieDBContext();
 
         // GET: Movies
-        public ActionResult Index(string searchString)
+        public ActionResult Index(string movieGenre, string searchString)
         {
+            var GenreLst = new List<string>();
+
+            var GenreQry = from d in db.Movies
+                           orderby d.Genre
+                           select d.Genre;
+
+            GenreLst.AddRange(GenreQry.Distinct());
+            ViewBag.movieGenre = new SelectList(GenreLst);
+
             var movies = from m in db.Movies
                          select m;
 
@@ -25,7 +34,12 @@ namespace WebApplication1.Controllers
                 movies = movies.Where(s => s.Title.Contains(searchString));
             }
 
-            return View(movies); 
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            return View(movies);
         }
 
         // GET: Movies/Details/5
---
+++ iBuy/iBuy/iBuy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="iBuy.MvcApplication" Language="C#" %>
