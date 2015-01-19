65a557a16adbb06d1da2ee1314813bddb461450b
jin benli <benli0822@gmail.com>
2015-01-19 09:21:07 +0100

    Added Address and Announce Entity

---
+++ iBuy/iBuy/iBuy/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace iBuy.Models
+{
+    public class Address
+    {
+        // Id
+        public int Id { get; set; }
+
+        // Address's name
+        public string Name { get; set; }
+
+        // Address' postal code
+        public string PostalCode { get; set; }
+    }
+}
---
+++ iBuy/iBuy/iBuy/Models/Announce.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Owin.BuilderProperties;
+
+namespace iBuy.Models
+{
+    public class Announce
+    {
+        // Attribut Id
+        public int Id { get; set; }
+
+        // An annouce's description
+        public string Description { get; set; }
+
+        // The title
+        public string Title { get; set; }
+
+        // Price for the article
+        public float Price { get; set; }
+
+        // Publish time
+        public DateTime Date { get; set; }
+
+        // Is a professional annouce
+        public bool Isprof { get; set; }
+
+        // A demand or an offer
+        public int Type { get; set; }
+
+        // The annoucement's address
+        public Address Address { get; set; }
+
+        // Publisher
+        public User User { get; set; }
+
+        // Category
+        public Category Category { get; set; }
+    }
+}
