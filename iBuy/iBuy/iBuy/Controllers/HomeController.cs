2426d361a5912c4bd2587263a07409b52f089ef9
jin benli <benli0822@gmail.com>
2015-01-25 20:47:01 +0100

    Index page modified and uncover title href

--- iBuy/iBuy/iBuy/Controllers/HomeController.cs
+++ iBuy/iBuy/iBuy/Controllers/HomeController.cs
@@ -1,16 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using iBuy.Models;
 
 namespace iBuy.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private ApplicationDbContext db = null;
+
+        public HomeController()
         {
-            return View();
+            db = new ApplicationDbContext();
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Announces.ToListAsync());
+        }
+
+        // GET: Announces/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Announce announce = db.Announces.Find(id);
+            if (announce == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(announce);
         }
 
         public ActionResult About()
